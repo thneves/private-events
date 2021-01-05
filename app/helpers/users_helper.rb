2f465150c668238e9140a226fbf43ea8dba10a0b
bigwizzo <wnnwillard@gmail.com>
2021-01-06 00:13:35 +0200

    Refactor code according to requested changes

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
   def log_structure
     if logged_in?
-      # puts current_user.username
+      # current_user.username
       link_to 'Log Out', session_path(session[:username]), method: :delete
     else
       link_to 'Log In', new_session_path
